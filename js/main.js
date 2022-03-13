e4e25c6fcde6a048e049ea3a982809f47b6a04af
Jaffar Hussein <jaffhussein@icloud.com>
2022-03-13 09:18:19 +0100

    ft:portfolio-added the js logic for the portfolio page

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // design toggle image
-    $('#design_icon,#design').click(function(){
+    $('#design_icon,#design').click(function () {
         $('#design_icon').toggle();
         $('#design').toggle();
     })
     // development toggle
-    $('#dev_icon,#development').click(function(){
+    $('#dev_icon,#development').click(function () {
         $('#dev_icon').toggle();
         $('#development').toggle();
     })
     // Product management
-    $('#products_icon,#products').click(function(){
+    $('#products_icon,#products').click(function () {
         $('#products').toggle();
         $('#products_icon').toggle();
 
     })
     // Portfolio Page Functionality
-    $
-
+    $('#portfolio').children().children().hover(function(){
+        $('img',this).toggleClass('blurry_effect');
+        $('p',this).toggle();
+        
+    })
 })
